8633cabf5f8d24ee68bbefc94af00bc32e601584
CuveeCui <18280133086@163.com>
2021-01-22 16:57:30 +0800

    update: asyncAsyncHook

--- examples/asyncAsyncHook.js
+++ examples/asyncAsyncHook.js
@@ -12,6 +12,38 @@ hook.tapAsync("BarPlugin", (name, age, cb) => {
 	cb();
 });
 
+hook.tap("HelloPlugin", (name, age) => {
+	console.log("HelloPlugin ---->", name, age);
+});
+
+hook.tapPromise("WorldPlugin", async (name, age) => {
+	console.log("WorldPlugin ---->", name, age);
+});
+
+// (function anonymous(name, age, _callback) {
+// 	"use strict";
+// 	var _context;
+// 	var _x = this._x;
+// 	function _next0() {
+// 		var _fn1 = _x[1];
+// 		_fn1(name, age, function(_err1) {
+// 			if (_err1) {
+// 				_callback(_err1);
+// 			} else {
+// 				_callback();
+// 			}
+// 		});
+// 	}
+// 	var _fn0 = _x[0];
+// 	_fn0(name, age, function(_err0) {
+// 		if (_err0) {
+// 			_callback(_err0);
+// 		} else {
+// 			_next0();
+// 		}
+// 	});
+// });
+
 hook.callAsync("cuvee", 30, e => {
 	console.log(e);
 });
